b94b8efb8642bec08832c42b4c5bf39366de3517
SeungHo Hwang <hoho7013@gmail.com>
2021-12-19 00:49:38 +0900

    second commit

--- Hello_git.cpp
+++ Hello_git.cpp
@@ -13,4 +13,4 @@ int main(void)
 //컨파일 : ctrl + Alt + C
 //실행 : ctrl + Alt + R
 //first commit : 인코딩 (EUC-kr)   = 실행창 정상 / github 오류
-//second commit : 인코딩 (UTF-8)   = 실행창 오류 / github ??
+//second commit : 인코딩 (UTF-8)   = 실행창 오류 / github 정상
